2cbf90ebc7c91dede6e6760fd95c1b085627ed61
maksim <92439695+Makvkn@users.noreply.github.com>
2021-12-04 09:41:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+/*
+const h1 = document.querySelector("h1")
+const h1Width = h1.clientWidth
+const bodyWidth = document.body.clientWidth
+const ml = (bodyWidth - h1Width) / 2
+h1.style.marginLeft = ml + "px"*/
+
+
+
+ const js1 = document.getElementById("js1");
+ const js2 = document.getElementById("js2");
+
+js1.style.marginRight = 'auto'
+js1.style.marginTop = 15 + 'px'
+js1.style.marginLeft = 'auto'
+js1.style.width = 300 + 'px'
+js1.style.height = 300 + 'px'
+js1.style.backgroundColor = '#000'
+
+
+
+
+
+js2.style.width = 150 + 'px'
+js2.style.height = 150 + 'px'
+js2.style.backgroundColor = '#FC5C7D'
+js2.style.position = 'relative'
+js2.style.left = 75 + 'px'
+js2.style.top = 70 + 'px'
+
+
